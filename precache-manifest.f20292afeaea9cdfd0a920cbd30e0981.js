1af9d48eed203af7cbc5d0f1b2cc59891954b05f
Alex Perez <alexperez@iMac-de-Alex.local>
2019-11-14 14:27:17 +0100

    Updates

---
+++ precache-manifest.f20292afeaea9cdfd0a920cbd30e0981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb9057f52bcc4ccf9b12e83604b2b40",
+    "url": "/videoController/index.html"
+  },
+  {
+    "revision": "7c474aa7826b96225869",
+    "url": "/videoController/static/css/main.c7ffe2ea.chunk.css"
+  },
+  {
+    "revision": "d6e5855a6aba2432f23e",
+    "url": "/videoController/static/js/2.c6bcf358.chunk.js"
+  },
+  {
+    "revision": "7c474aa7826b96225869",
+    "url": "/videoController/static/js/main.1b55ea50.chunk.js"
+  },
+  {
+    "revision": "74ac94c6a836826e7496",
+    "url": "/videoController/static/js/runtime-main.b8415802.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/videoController/static/media/logo.25bf045c.svg"
+  }
+]);
